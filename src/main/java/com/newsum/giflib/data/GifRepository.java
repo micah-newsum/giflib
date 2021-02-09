da5eb239f18686a940238d31ce8521c5c7a64710
Micah Newsum <mnewsum@glenair.com>
2021-02-09 08:12:02 -0800

    Implement favorites backend.

--- src/main/java/com/newsum/giflib/data/GifRepository.java
+++ src/main/java/com/newsum/giflib/data/GifRepository.java
@@ -50,4 +50,18 @@ public class GifRepository
         }
         return gifs;
     }
+
+    public List<Gif> getAllFavorites()
+    {
+        List<Gif> gifs = new ArrayList<>();
+
+        for (Gif gif : ALL_GIFS)
+        {
+            if (gif.isFavorite())
+            {
+                gifs.add(gif);
+            }
+        }
+        return gifs;
+    }
 }
