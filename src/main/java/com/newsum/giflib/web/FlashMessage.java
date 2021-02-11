667651c1c11e1eba98d02807f9e5733f608fd75c
Micah Newsum <mnewsum@glenair.com>
2021-02-11 07:23:47 -0800

    Add getters and setters.

--- src/main/java/com/newsum/giflib/web/FlashMessage.java
+++ src/main/java/com/newsum/giflib/web/FlashMessage.java
@@ -14,4 +14,20 @@ public class FlashMessage
     {
         SUCCESS, FAILURE;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
